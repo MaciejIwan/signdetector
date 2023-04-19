68ca1c2e30fabcfc212bef96040c4cf97c04234c
Maciej Iwan <64867638+MaciejIwan@users.noreply.github.com>
2023-04-19 22:02:07 +0200

    add speedLimit whitelist

--- include/models/SpeedLimitSign.h
+++ include/models/SpeedLimitSign.h
@@ -5,15 +5,19 @@
 #ifndef SIGN_EDGE_METHOD_TEST_1_SPEEDLIMITSIGN_H
 #define SIGN_EDGE_METHOD_TEST_1_SPEEDLIMITSIGN_H
 
-
 #include "RoadSign.h"
+
+#define DEFAULT_SPEED_LIMIT 0
 
 class SpeedLimitSign : public RoadSign {
 private:
-    int limit;
-
+    int limit = DEFAULT_SPEED_LIMIT;
+    static constexpr const int VALID_LIMITS[] = {10, 30, 40, 50, 60, 70, 90, 100, 110, 120, 130, 140};
 public:
-    SpeedLimitSign(int l) : RoadSign(SignType::SPEED_LIMIT), limit(l) {}
+    //SpeedLimitSign(int l) : RoadSign(SignType::SPEED_LIMIT), limit(l) {}
+    SpeedLimitSign(int l) : RoadSign(SignType::SPEED_LIMIT) {
+        setLimit(l);
+    }
 
     std::string toString() override {
         return "Speed limit: " + std::to_string(limit);
@@ -23,8 +27,19 @@ public:
         return limit;
     }
 
-    void setLimit(int limit) {
-        SpeedLimitSign::limit = limit;
+    void setLimit(int l) {
+        bool valid_limit = false;
+        for (const auto &lim: VALID_LIMITS) {
+            if (l == lim) {
+                valid_limit = true;
+                break;
+            }
+        }
+        if (valid_limit) {
+            SpeedLimitSign::limit = l;
+        } else {
+            SpeedLimitSign::limit = DEFAULT_SPEED_LIMIT;
+        }
     }
 };
 
