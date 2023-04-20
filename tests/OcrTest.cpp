126bb1e12e93fa6b2e996fe0a01315807de7f916
Maciej Iwan <64867638+MaciejIwan@users.noreply.github.com>
2023-04-20 20:20:39 +0200

    extract ocr test method

--- tests/OcrTest.cpp
+++ tests/OcrTest.cpp
@@ -6,18 +6,25 @@
 #include <string>
 #include <opencv2/imgcodecs.hpp>
 #include "../include/Ocr.h"
-#include "../include/models/SpeedLimitSign.h"
 
-TEST(OcrTest, shouldReturn40) {
-    std::cout << "OcrTest: ImagePreprocessing" << std::endl;
-    std::string filename = "imgs/signs/40_1.jpg";
-
+void testTextReading(const std::string &filename) {
     int expectedSign = 40;
     Ocr ocr = Ocr();
 
-    cv::Mat image = cv::imread(filename);
+    cv::Mat image = cv::imread("../test_images/" + filename);
     int actualSign = ocr.getNumberFromRoi(image);
 
     ASSERT_EQ(expectedSign, actualSign);
 }
 
+TEST(OcrTest, shouldReturn40)
+{
+    std::string filename = "40_1.png";
+    testTextReading(filename);
+}
+
+TEST(OcrTest2, shouldReturn40)
+{
+    std::string filename = "40_2.png";
+    testTextReading(filename);
+}
