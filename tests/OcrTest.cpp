8130abe3277ab08d6eea98fdfb46846e26458033
Maciej Iwan <64867638+MaciejIwan@users.noreply.github.com>
2023-04-21 03:01:11 +0200

    group TEST

--- tests/OcrTest.cpp
+++ tests/OcrTest.cpp
@@ -7,86 +7,87 @@
 #include <opencv2/imgcodecs.hpp>
 #include "../include/Ocr.h"
 
-void testTextReading(const std::string &filename) {
-    //given
-    Ocr ocr = Ocr();
-    int expectedSpeedLimit = std::stoi(filename);
+class RoiOcrTest : public ::testing::Test {
+public:
+    static void testTextReading(const std::string &filename) {
+        //given
+        Ocr ocr = Ocr();
+        int expectedSpeedLimit = std::stoi(filename);
 
-    //when
-    cv::Mat image = cv::imread("../resources/imgs/signs/" + filename);
-    int actualSpeedLimit = ocr.getNumberFromRoi(image);
+        //when
+        cv::Mat image = cv::imread("../resources/imgs/signs/" + filename);
+        int actualSpeedLimit = ocr.getNumberFromRoi(image);
 
-    // then
-    ASSERT_EQ(expectedSpeedLimit, actualSpeedLimit);
-}
+        // then
+        ASSERT_EQ(expectedSpeedLimit, actualSpeedLimit);
+    }
+protected:
+    void SetUp() override {
+    }
+
+    void TearDown() override {
+    }
+};
 
 
-TEST(shouldReturn30_1, OcrTest) {
+
+
+TEST_F(RoiOcrTest, shouldReturn30_1) {
     std::string filename = "30_1.jpg";
     testTextReading(filename);
 }
 
-TEST(shouldReturn30_2, OcrTest) {
+TEST_F(RoiOcrTest, shouldReturn30_2) {
     std::string filename = "30_2.jpg";
     testTextReading(filename);
 }
 
-TEST(shouldReturn30_3, shouldReturn30) {
+TEST_F(RoiOcrTest, shouldReturn30_3) {
     std::string filename = "30_3.jpg";
     testTextReading(filename);
 }
 
-TEST(shouldReturn40_1, shouldReturn40) {
+TEST_F(RoiOcrTest, shouldReturn40_1) {
     std::string filename = "40_1.jpg";
     testTextReading(filename);
 }
 
-TEST(shouldReturn40_2, shouldReturn40) {
-    std::string filename = "40_2.jpg";
-    testTextReading(filename);
-}
-
-TEST(shouldReturn40_3, shouldReturn40) {
-    std::string filename = "40_3.jpg";
-    testTextReading(filename);
-}
-
-TEST(shouldReturn40_4, shouldReturn40) {
+TEST_F(RoiOcrTest, shouldReturn40_4) {
     std::string filename = "40_4.jpg";
     testTextReading(filename);
 }
 
-TEST(shouldReturn40_5, shouldReturn40) {
+TEST_F(RoiOcrTest, shouldReturn40_5) {
     std::string filename = "40_5.jpg";
     testTextReading(filename);
 }
 
-TEST(shouldReturn50_1, shouldReturn50) {
+TEST_F(RoiOcrTest, shouldReturn50_1) {
     std::string filename = "50_1.jpg";
     testTextReading(filename);
 }
 
-TEST(shouldReturn60_1, shouldReturn60) {
+TEST_F(RoiOcrTest, shouldReturn60_1) {
     std::string filename = "60_1.jpg";
     testTextReading(filename);
 }
 
-TEST(shouldReturn60_2, shouldReturn60) {
+TEST_F(RoiOcrTest, shouldReturn60_2) {
     std::string filename = "60_2.jpg";
     testTextReading(filename);
 }
 
-TEST(shouldReturn70_1, shouldReturn70) {
+TEST_F(RoiOcrTest, shouldReturn70_1_blackBackground) {
     std::string filename = "70_1.jpg";
     testTextReading(filename);
 }
 
-TEST(shouldReturn70_2, shouldReturn70) {
+TEST_F(RoiOcrTest, shouldReturn70_2_strongReflation) {
     std::string filename = "70_2.jpg";
     testTextReading(filename);
 }
 
-TEST(shouldReturn70_3, shouldReturn70) {
+TEST_F(RoiOcrTest, shouldReturn70_3) {
     std::string filename = "70_3.jpg";
     testTextReading(filename);
 }
--- tests/VideoSeqenceTests.cpp
+++ tests/VideoSeqenceTests.cpp
@@ -3,13 +3,23 @@
 #include <gtest/gtest.h>
 #include <string>
 
-TEST(VideoTest_speed_limit_seqence_1, SignRecognitionAccuracy)
+
+class VideoSequenceTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+    }
+
+    void TearDown() override {
+    }
+};
+
+TEST_F(VideoSequenceTest, VideoTest_speed_limit_seqence_1)
 {
     std::string filename = "speed_limit_seqence_1";
     testSignRecognitionAccuracy(filename);
 }
 
-TEST(VideoTest_speed_limit_11, SignRecognitionAccuracy)
+TEST_F(VideoSequenceTest, VideoTest_speed_limit_11)
 {
     std::string filename = "speed_limit_11";
     testSignRecognitionAccuracy(filename);
