230d5115513b8d51deb6db37d5eb00c2a6d0402a
Daniel Chróst <danielchrost165@gmail.com>
2023-06-01 19:22:28 +0200

    App class added

---
+++ include/App.h
@@ -0,0 +1,14 @@
+//
+// Created by daniel on 01.06.23.
+//
+
+#ifndef SIGN_DETECTOR_APP_H
+#define SIGN_DETECTOR_APP_H
+
+
+class App {
+
+};
+
+
+#endif //SIGN_DETECTOR_APP_H
---
+++ src/App.cpp
@@ -0,0 +1,5 @@
+//
+// Created by daniel on 01.06.23.
+//
+
+#include "App.h"
